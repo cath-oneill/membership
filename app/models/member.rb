a98abe4c8147e11e5a1475703e820ac141be8a8e
Catherine O'Neill <cathzozo@gmail.com>
2014-10-20 14:44:06 -0500

    added membership boolean methods to member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,20 @@
 class Member < ActiveRecord::Base
   has_many :payments
+
+  def current_member?(date_string = nil)
+    date = set_date(date_string)
+    self.payments.where(date: date-365..date).present?
+  end
+
+  def paid_within_two_years?(date_string = nil)
+    date = set_date(date_string)
+    self.payments.where(date: date-730..date).present?
+  end
+
+  private
+
+  def set_date(date_string)
+    return Date.today if date_string.nil?
+    Date.parse(date_string)
+  end  
 end

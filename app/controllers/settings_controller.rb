ff198d3c97f7db021e370161084f462af9327484
Catherine O'Neill <coneill@academicworks.com>
2015-02-12 20:53:01 -0600

    settings form controller now adjustable to new settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,25 +1,26 @@
 class SettingsController < ApplicationController
 
   def index
-    @club_name = Setting.where(lookup: "club_name").first
-    @other_clubs = Setting.where(lookup: "other_clubs").first
+    @settings = Setting.all
   end
 
   # PATCH/PUT /members/:member_id/notes/1
   def update
-    @club_name = Setting.where(lookup: "club_name").first
-    @other_clubs = Setting.where(lookup: "other_clubs").first
-    if setting_params[:club_name]
-      @club_name.update(value: setting_params[:club_name])
-    elsif setting_params[:other_clubs]      
-      @other_clubs.update(value: setting_params[:other_clubs])
+    p params
+    p setting_params
+    setting_params.each do |key, value| 
+      setting = Setting.find_by(lookup: key.to_s).update(value: value)
     end
-    redirect_to @settings, notice: 'Settings were updated'
+    redirect_to settings_path, notice: 'Settings were updated'
   end
 
   private
     # Only allow a trusted parameter "white list" through.
     def setting_params
-      params.require(:setting).permit(:club_name, :other_clubs)
+      params.require(:setting).permit(setting_lookup_values)
+    end
+
+    def setting_lookup_values
+      Setting.pluck(:lookup).map{|x| x.to_sym}
     end
 end
